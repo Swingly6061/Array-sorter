952ec8e402968ea332b1e8a0d419f60443f00c79
Swingly6061 <50369014+Swingly6061@users.noreply.github.com>
2019-05-27 14:46:55 +0200

    Fixed calculations

--- ArraySorter.java
+++ ArraySorter.java
@@ -1,6 +1,11 @@
-// 
+// Sorts array of random values.
 public class ArraySorter {
 
 	public static void main(String[] args) {
-    
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math .random() * 100);
+            System.out.println(array[i]);
+        }
     }
+}
